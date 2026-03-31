1772bca8a5dc94c5b4773bfcab4bca176ea469d4
Siku0415 <vipulsharma8764@gmail.com>
2026-04-01 02:39:30 +0530

    Update download.js

--- app/applet/download.js
+++ app/applet/download.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import https from 'https';
 
-https.get('https://www.ratangames.com/assets/index-5IOwuUrr.js', (res) => {
+https.get('https://ratangames.com/Janta555.apk?text=Hi,%20I%20need%20help%20with%20Janta555', (res) => {
   let data = '';
   res.on('data', (chunk) => {
     data += chunk;
